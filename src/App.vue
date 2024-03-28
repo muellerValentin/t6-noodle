8a6b16188f8ada90a8eb676768fbeebdde7f75aa
Daniel <daniel.vollmer@bechtle.com>
2024-03-28 17:22:00 +0100

    firebase init

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,23 @@
 </template>
 
 <script setup>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCupHhjqvoXWDZwTv1Aa_xZY9MVrWmiH_U",
+  authDomain: "noodle-t6.firebaseapp.com",
+  projectId: "noodle-t6",
+  storageBucket: "noodle-t6.appspot.com",
+  messagingSenderId: "427139124965",
+  appId: "1:427139124965:web:0f3ca972889e350ebf8e0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 defineOptions({
   name: 'App'
 });
