a9c0ad493dd5499636fa67618cf379d60cc43eb1
muellerValentin <mueller.valentin@icloud.com>
2024-04-01 01:22:59 +0200

    button for checking nfc support

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <router-view />
+
+  <!-- NFC check -->
+  <button :class="{ supported: nfcSupported, 'not-supported': !nfcSupported }">
+    NFC Check
+  </button>
 </template>
 
 <script setup>
 // Import the functions you need from the SDKs you need
+import { ref } from "vue";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,12 +21,12 @@ const firebaseConfig = {
   projectId: "noodle-t6",
   storageBucket: "noodle-t6.appspot.com",
   messagingSenderId: "427139124965",
-  appId: "1:427139124965:web:0f3ca972889e350ebf8e0c"
+  appId: "1:427139124965:web:0f3ca972889e350ebf8e0c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 defineOptions({
-  name: 'App'
+  name: "App",
 });
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,25 +11,22 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <!-- NFC check -->
+      <button
+        :class="{ supported: nfcSupported, 'not-supported': !nfcSupported }"
+      >
+        NFC Check
+      </button>
+
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink
           v-for="link in linksList"
@@ -46,61 +43,85 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+// NFC check
+if ("NDEFReader" in window) {
+  console.log("NFC API supported");
+} else {
+  console.log("NFC API not supported in this browser");
+}
+
+const nfcSupported = ref(false);
+if ("NDEFReader" in window) {
+  nfcSupported.value = true;
 }
 </script>
+
+<style scoped>
+.supported {
+  background-color: green;
+  z-index: 9999;
+}
+
+.not-supported {
+  background-color: red;
+  z-index: 9999;
+}
+</style>
